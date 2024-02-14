8776211cc28035878cf8eb7cd6988aec68f9420e
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-14 19:53:18 +0100

    Force version

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 mkdir -p build
 pushd build
-cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$(pwd)/dist ..
+cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$(pwd)/dist -DDUCKDB_FORCE_VERSION=$PKG_VERSION ..
 ninja
 ninja install
