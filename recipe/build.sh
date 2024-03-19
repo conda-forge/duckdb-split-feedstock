e3f894db0d54e723cd5beaff9f7dbcabae4ec034
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-19 09:20:40 +0100

    Remove version patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DDUCKDB_PLATFORM=osx_arm64 -DDUCKDB_EXPLICIT_PLATFORM=osx_arm64"
 fi
 
-cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$(pwd)/dist -DDUCKDB_FORCE_VERSION=$PKG_VERSION ..
+cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$(pwd)/dist -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g4a89d97 ..
 
 ninja
 ninja install
