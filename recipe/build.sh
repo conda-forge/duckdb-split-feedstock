c7e5546d0b744619cfef12b61e3079190a15e3a4
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2024-05-03 11:58:50 +0200

    Add first test extension.

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,24 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DDUCKDB_PLATFORM=osx_arm64 -DDUCKDB_EXPLICIT_PLATFORM=osx_arm64"
 fi
 
-cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$(pwd)/dist -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g4a89d97 ..
+if [ "${target_platform}" = "linux-64" ]; then
+    DUCKDB_ARCH='linux_amd64'
+elif [ "${target_platform}" = "osx-64" ]; then
+    DUCKDB_ARCH='osx_amd64'
+elif [ "${target_platform}" = "osx-arm64" ]; then
+    DUCKDB_ARCH='osx_arm64'
+else
+    echo "Unknown target platform: ${target_platform}"
+    exit 1
+fi
+
+# Persist DuckDB architecture for extension installation scripts.
+echo "${DUCKDB_ARCH}" > "$(pwd)"/.duckdb_arch
+
+BUILD_EXTENSIONS='json;httpfs'
+SKIP_EXTENSIONS='parquet;jemalloc'
+
+cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$(pwd)/dist -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g4a89d97 -DBUILD_EXTENSIONS="${BUILD_EXTENSIONS}" -DSKIP_EXTENSIONS="${SKIP_EXTENSIONS}" ..
 
 ninja
 ninja install
---
+++ recipe/install-extension-json.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# TODO: Retrieve the extension name dynamically.
+EXTENSION_NAME='json'
+
+DUCKDB_ARCH="$(cat ./build/.duckdb_arch)"
+DUCKDB_VERSION="v${PKG_VERSION}"
+
+mkdir -p "${PREFIX}"/duckdb/extensions/"${DUCKDB_VERSION}"/"${DUCKDB_ARCH}"/
+
+cp ./build/repository/"${DUCKDB_VERSION}"/"${DUCKDB_ARCH}"/"${EXTENSION_NAME}".duckdb_extension "${PREFIX}"/duckdb/extensions/"${DUCKDB_VERSION}"/"${DUCKDB_ARCH}"/
