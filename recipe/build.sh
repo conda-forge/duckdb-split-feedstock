6bb9db42db1288da61e51d872040c4577824b1f7
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2024-05-03 14:41:18 +0200

    Use ==.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,11 @@ set -euxo pipefail
 mkdir -p build
 pushd build
 
-if [ "${target_platform}" = "linux-64" ]; then
+if [[ "${target_platform}" == "linux-64" ]]; then
     DUCKDB_ARCH='linux_amd64'
-elif [ "${target_platform}" = "osx-64" ]; then
+elif [[ "${target_platform}" == "osx-64" ]]; then
     DUCKDB_ARCH='osx_amd64'
-elif [ "${target_platform}" = "osx-arm64" ]; then
+elif [[ "${target_platform}" == "osx-arm64" ]]; then
     DUCKDB_ARCH='osx_arm64'
     export CMAKE_ARGS="${CMAKE_ARGS} -DDUCKDB_PLATFORM=osx_arm64 -DDUCKDB_EXPLICIT_PLATFORM=osx_arm64"
 else
