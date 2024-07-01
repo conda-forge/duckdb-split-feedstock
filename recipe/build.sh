e5b398a081e60c0f30a096558da90961b4e3de04
Uwe L. Korn <xhochy@users.noreply.github.com>
2024-07-01 18:50:15 +0200

    There is no more excel

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,7 +27,7 @@ fi
 # Persist DuckDB architecture for extension installation scripts.
 echo "${DUCKDB_ARCH}" > "$(pwd)/.duckdb_arch"
 
-BUILD_EXTENSIONS='json;httpfs;autocomplete;excel;fts;icu;inet;tpcds;tpch'
+BUILD_EXTENSIONS='json;httpfs;autocomplete;fts;icu;inet;tpcds;tpch'
 # We skip the parquet extension because it's compiled into libduckdb by default.
 SKIP_EXTENSIONS='parquet;jemalloc'
 
