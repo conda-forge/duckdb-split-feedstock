edc780deb1302b818a55cab424f09b2dd600c2f9
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-03-31 20:23:29 +0100

    Update the test for builtin extensions to ignore the jemalloc extension on Linux

--- recipe/test-builtin-extensions.sh
+++ recipe/test-builtin-extensions.sh
@@ -3,9 +3,10 @@ set -euxo pipefail
 PKG_PREFIX='duckdb-extension-'
 EXTENSION_NAME="${PKG_NAME#$PKG_PREFIX}"
 
-QUERY="select extension_name from duckdb_extensions() where (installed and install_mode = 'STATICALLY_LINKED');"
+# jemalloc is statically linked into DuckDB only on the Linux platform. Skip it here for simplicity.
+QUERY="select extension_name from duckdb_extensions() where (installed and install_mode = 'STATICALLY_LINKED' and extension_name <> 'jemalloc');"
 
-# Check that the built-in extensions are as expected. 
+# Check that the built-in extensions are as expected.
 duckdb -json -c "${QUERY}" | jq -r -e '
   map(.extension_name) as $actual
   | (["autocomplete", "core_functions", "icu", "json", "parquet", "shell"] | sort) as $expected
