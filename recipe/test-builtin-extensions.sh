cf7f557137db76cd0befeaeaa907bea5fcd2d4b7
Uwe L. Korn <uwe.korn@quantco.com>
2025-04-01 16:46:13 +0200

    jemalloc check is linux-only

--- recipe/test-builtin-extensions.sh
+++ recipe/test-builtin-extensions.sh
@@ -3,12 +3,18 @@ set -euxo pipefail
 PKG_PREFIX='duckdb-extension-'
 EXTENSION_NAME="${PKG_NAME#$PKG_PREFIX}"
 
-# jemalloc is statically linked into DuckDB only on the Linux platform. Skip it here for simplicity.
-QUERY="select extension_name from duckdb_extensions() where (installed and install_mode = 'STATICALLY_LINKED' and extension_name <> 'jemalloc');"
+if [[ "${target_platform}" == linux-* ]]; then
+  # jemalloc is statically linked into DuckDB only on the Linux platform.
+  EXPECTED_EXTENSIONS='"autocomplete", "core_functions", "icu", "jemalloc", "json", "parquet", "shell"'
+else
+  EXPECTED_EXTENSIONS='"autocomplete", "core_functions", "icu", "json", "parquet", "shell"'
+fi
+
+QUERY="select extension_name from duckdb_extensions() where (installed and install_mode = 'STATICALLY_LINKED');"
 
 # Check that the built-in extensions are as expected.
-duckdb -json -c "${QUERY}" | jq -r -e '
-  map(.extension_name) as $actual
-  | (["autocomplete", "core_functions", "icu", "json", "parquet", "shell"] | sort) as $expected
-  | ($actual | sort) == $expected
-'
+duckdb -json -c "${QUERY}" | jq -r -e "
+  map(.extension_name) as \$actual
+  | ([${EXPECTED_EXTENSIONS}] | sort) as \$expected
+  | (\$actual | sort) == \$expected
+"
