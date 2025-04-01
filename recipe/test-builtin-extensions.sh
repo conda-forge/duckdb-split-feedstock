46af410e43244ace2842812e36fbe918170a92bf
Uwe L. Korn <uwe.korn@quantco.com>
2025-04-01 18:43:02 +0200

    jemalloc is also on linux-aarch64

--- recipe/test-builtin-extensions.sh
+++ recipe/test-builtin-extensions.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 PKG_PREFIX='duckdb-extension-'
 EXTENSION_NAME="${PKG_NAME#$PKG_PREFIX}"
 
-if [[ "${target_platform}" == linux-64 ]]; then
+if [[ "${target_platform}" == linux-* ]]; then
   # jemalloc is statically linked into DuckDB only on the Linux platform.
   EXPECTED_EXTENSIONS='"autocomplete", "core_functions", "icu", "jemalloc", "json", "parquet", "shell"'
 else
