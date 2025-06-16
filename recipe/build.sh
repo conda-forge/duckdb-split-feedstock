1a3eca094ed5a8655b001900cd765249d4faf5cb
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-06-16 20:01:13 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,7 +53,7 @@ EOF
 cmake ${CMAKE_ARGS} \
     -GNinja \
     -DCMAKE_INSTALL_PREFIX=$(pwd)/dist \
-    -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-2063dda \
+    -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g2063dda \
     -DDUCKDB_EXTENSION_CONFIGS="$PWD/bundled_extensions.cmake" \
     -DWITH_INTERNAL_ICU=OFF \
     ..
