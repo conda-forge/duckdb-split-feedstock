92537241f77b2effe825a2652cc657cc512528ef
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-06-18 12:18:17 +0100

    Fix variable syntax in build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,8 +2,8 @@ python scripts/windows_ci.py
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_GENERATOR_PLATFORM=x64 \
-    -DDUCKDB_EXTENSION_CONFIGS="$PWD/bundled_extensions.cmake" \
+    -DDUCKDB_EXTENSION_CONFIGS="%CD%/bundled_extensions.cmake" \
     -DDISABLE_UNITY=1 \
-    -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g2063dda \
+    -DOVERRIDE_GIT_DESCRIBE=v%PKG_VERSION%-0-g2063dda \
     -DWITH_INTERNAL_ICU=OFF
 cmake --build . --config Release --parallel
