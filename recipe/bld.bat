eabb4e14ee3d68c479506c504f34f905cc08d83e
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-06-18 13:58:48 +0100

    Add bundled extensions configuration to build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,25 @@
 
 set OVERRIDE_GIT_DESCRIBE=v%PKG_VERSION%-0-g2063dda
 
+:: This is the extension config that is used to build / test
+(
+echo #
+echo ## Extensions that are linked
+echo #
+echo duckdb_extension_load^(icu^)
+echo duckdb_extension_load^(json^)
+echo duckdb_extension_load^(parquet^)
+echo duckdb_extension_load^(autocomplete^)
+echo.
+echo #
+echo ## Extensions that are not linked
+echo #
+echo duckdb_extension_load^(tpcds DONT_LINK^)
+echo duckdb_extension_load^(tpch DONT_LINK^)
+echo duckdb_extension_load^(httpfs DONT_LINK^)
+echo duckdb_extension_load^(fts DONT_LINK^)
+) > "%CD%\bundled_extensions.cmake"
+
 python scripts/windows_ci.py
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
--- recipe/build.sh
+++ recipe/build.sh
@@ -33,6 +33,25 @@ export OPENSSL_ROOT_DIR="${PREFIX}"
 
 export OVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g2063dda
 
+# This is the extension config that is used to build / test
+cat > $PWD/bundled_extensions.cmake <<EOF
+#
+## Extensions that are linked
+#
+duckdb_extension_load(icu)
+duckdb_extension_load(json)
+duckdb_extension_load(parquet)
+duckdb_extension_load(autocomplete)
+
+#
+## Extensions that are not linked
+#
+duckdb_extension_load(tpcds DONT_LINK)
+duckdb_extension_load(tpch DONT_LINK)
+duckdb_extension_load(httpfs DONT_LINK)
+duckdb_extension_load(fts DONT_LINK)
+EOF
+
 cmake ${CMAKE_ARGS} \
     -GNinja \
     -DCMAKE_INSTALL_PREFIX=$(pwd)/dist \
