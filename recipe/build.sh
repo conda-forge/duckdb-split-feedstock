f175c76bf6f6c9f6d44e668aa6c282afe840378c
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2026-05-21 02:43:40 +0100

    Update OVRERRIDE_GIT_DESCRIBE to new commit hash in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -73,7 +73,7 @@ EOF
 cmake ${CMAKE_ARGS} \
     -GNinja \
     -DCMAKE_INSTALL_PREFIX=$(pwd)/dist \
-    -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g8a58519 \
+    -DOVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g14eca11 \
     -DDUCKDB_EXTENSION_CONFIGS="$PWD/bundled_extensions.cmake" \
     -DWITH_INTERNAL_ICU=OFF \
     ..
