2a949db2d5ccedbff9f0d3a8cbf80e0ff93a9b73
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2026-05-21 04:27:39 +0100

    Simplify EXPECTED_EXTENSIONS assignment
    Drop conditional assignment of EXPECTED_EXTENSIONS

--- recipe/test-builtin-extensions.sh
+++ recipe/test-builtin-extensions.sh
@@ -2,10 +2,7 @@ set -euxo pipefail
 
 PKG_PREFIX='duckdb-extension-'
 EXTENSION_NAME="${PKG_NAME#$PKG_PREFIX}"
-
-if [[ "${target_platform}" == linux-* ]]; then
-  EXPECTED_EXTENSIONS='"autocomplete", "core_functions", "icu", "json", "parquet", "shell"'
-fi
+EXPECTED_EXTENSIONS='"autocomplete", "core_functions", "icu", "json", "parquet", "shell"'
 
 QUERY="select extension_name from duckdb_extensions() where (installed and install_mode = 'STATICALLY_LINKED');"
 
